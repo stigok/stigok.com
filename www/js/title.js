41f892bd49d5c7086e6ea52f3c957f3c0b4ad468
Stig Otnes Kolstad <stig@stigok.com>
2017-07-03 20:33:11 +0200

    Do some love

--- www/js/title.js
+++ www/js/title.js
@@ -1,9 +1,17 @@
-function showSomeLove (document) {
-  const love = ' <3'
-  setInterval(() => {
-    document.title += love
-    setTimeout(() => {
-      document.title = document.title.substr(0, document.title.length - love.length)
-    }, 1000)
-  }, 2000)
+function look4luv (find, the, love, and, u, will, know) {
+  if (--I <3) return
+  the = find ? find.document : u || will || know || the || love
+  love = '<3'
+  find = (u || know || the || love)
+  u = undefined
+
+  u ?
+  will :
+  (find || the).title = the.title.endsWith(love) ? the.title.substr(0, the.title.length - love.length) : the.title +
+  love
+
+//so
+  setTimeout(function (and) {
+    look4luv(and, the, love, will, find, u)
+  }, 420)
 }
